a75af2eba5a0c3831b401e2ec100e4bcafa3c568
anaMariaFelix <anafelix0909@gmail.com>
2024-09-18 19:45:21 -0300

    primeiro commit

---
+++ src/questoes/Questao2.java
@@ -0,0 +1,25 @@
+package questoes;
+
+import java.util.Scanner;
+
+public class Questao2 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe uma palavra ou frase: ");
+		String texto = entrada.nextLine();	
+
+		int cont = 0;
+		boolean existeLetraA = texto.contains("a");
+		
+		for(int i = 0; i < texto.length();i ++) {
+			if(texto.charAt(i) == 'a' || texto.charAt(i) == 'A') {
+				cont ++;
+			}
+		}
+		System.out.println(existeLetraA == true? "Lentra 'A' existente": "Lentra 'A' não existente");
+		System.out.print("Quantidade de letras 'A': " + cont );
+	}
+
+}
---
+++ src/questoes/questao1.java
@@ -0,0 +1,31 @@
+package questoes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class questao1 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe um número: ");
+		int numero = Integer.parseInt(entrada.nextLine());
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		
+		int valor1 = 0;
+		int valor2 = 1;  //(0, 1, 1, 2, 3, 5, 8, 13, 21, 34...)
+		
+		lista.add(valor1);
+		lista.add(valor2);
+		int aux;
+		for(int i = 0; i < numero; i++) {
+			aux = lista.get(i) + lista.get( i +1);
+			lista.add(aux);
+		}
+		
+		for(int i = 0; i < lista.size(); i++) {
+			System.out.print(lista.get(i) + ",");
+		}
+		System.out.println();
+		System.out.print(lista.contains(numero)? "o numero pertence a sequencia.": "o numero não pertence a sequencia.");
+	}
+}
